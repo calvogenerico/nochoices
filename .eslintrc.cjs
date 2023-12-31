91a903f4a5e68190316e823ffad019dc15f65272
migue <miguelfeliped@gmail.com>
2023-12-31 15:20:18 -0300

    little improvements

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,16 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint'
-    ],
-    ignorePatterns: ['docs/**/*', 'dist/**/*'],
-    root: true,
-    rules: {
-        "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
-    }
-};
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint'
+  ],
+  ignorePatterns: ['docs/**/*', 'dist/**/*', '*.js'],
+  root: true,
+  rules: {
+    // "tsdoc/syntax": "warn",
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
+  }
+}
